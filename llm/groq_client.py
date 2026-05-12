db40389548d8dc244d77378f76beebec7f9c05a1
Vishok Sharma <vishokmait@gmail.com>
2026-05-12 17:51:23 +0530

    add groq completion method

--- llm/groq_client.py
+++ llm/groq_client.py
@@ -5,3 +5,9 @@ class GroqClient(ChatGroq):
 
     def __init__(self, api_key):
 
+        super().__init__(
+            model="qwen/qwen3-32b",
+            api_key=api_key,
+            temperature=0,
+            reasoning_effort="none"
+        )
