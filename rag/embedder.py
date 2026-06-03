74de127bb6b2409fcc2aa4d822f3970d146047bd
Vishok Sharma <vishokmait@gmail.com>
2026-06-03 10:55:50 +0530

    complete embedding request handler

--- rag/embedder.py
+++ rag/embedder.py
@@ -7,3 +7,11 @@ class Embedder:
     def __init__(self):
         pass
     
+    def embed(self,text):
+        
+        response = ollama.embeddings(
+            model= self.MODEL_NAME,
+            prompt = text
+        )
+    
+        return response["embedding"]
