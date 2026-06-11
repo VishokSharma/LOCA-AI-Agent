ac3e1e1fab1575ebbd2d5d1c73f351c5d8f2df8a
Vishok Sharma <vishokmait@gmail.com>
2026-06-11 20:50:37 +0530

    finish filesystem tools implementation

--- tools/filesystem.py
+++ tools/filesystem.py
@@ -159,3 +159,43 @@ class FileSystemTool:
             encoding="utf-8"
         )
 
+            return {
+            "success": True,
+            "path": str(path)
+        }
+
+        except Exception as e:
+
+            return {
+            "success": False,
+            "error": str(e)
+            }
+            
+    def append_file(
+    self,
+    path,
+    content
+):
+
+        try:
+
+            with open(
+            path,
+            "a",
+            encoding="utf-8"
+            ) as file:
+
+                file.write("\n" + content)
+
+            return {
+            "success": True,
+            "path": path,
+            "message": "Content appended successfully"
+            }
+
+        except Exception as e:
+
+            return {
+            "success": False,
+            "error": str(e)
+            }
