cede640cdaae0e2109a58d8e2968885b58d4ab4c
Vishok Sharma <vishokmait@gmail.com>
2026-07-02 17:19:14 +0530

    init tool registry aggregator

---
+++ langchain_tools/all_tools.py
@@ -0,0 +1,7 @@
+from langchain_tools.browser_tools import BROWSER_TOOLS
+from langchain_tools.desktop_tools import DESKTOP_TOOLS
+from langchain_tools.filesystem_tools import FILESYSTEM_TOOLS
+from langchain_tools.search_tools import SEARCH_TOOLS
+from langchain_tools.knowledge_tools import KNOWLEDGE_TOOLS
+
+ALL_TOOLS = (
