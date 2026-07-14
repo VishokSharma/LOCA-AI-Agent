fa9a68ecf397dadd37e412215df8b0f55581f612
Vishok Sharma <vishokmait@gmail.com>
2026-07-14 09:50:08 +0530

    complete audio playback handler

--- voice/player.py
+++ voice/player.py
@@ -14,3 +14,19 @@ class AudioPlayer:
     ):
 
         pygame.mixer.music.load(
+            audio_file
+        )
+
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+
+            pygame.time.Clock().tick(
+                30
+            )
+
+        pygame.mixer.music.stop()
+
+        pygame.mixer.music.unload()
+
+        time.sleep(0.2)
